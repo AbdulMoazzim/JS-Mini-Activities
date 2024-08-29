e8738c79ad9b334f1e92a0ee32cbcbdefa535383
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-08-30 00:01:28 +0500

    Finally Completed

--- Activity 4/script.js
+++ Activity 4/script.js
@@ -1,5 +1,8 @@
 let links = document.querySelectorAll('.links li');
 let divs = document.querySelectorAll('.product');
+let ham = document.querySelector("#menu");
+let login = document.querySelector(".login");
+let close = document.querySelector("#close");
 
 links.forEach((val, index)=>{
     val.addEventListener('mouseenter',()=>{
@@ -8,4 +11,16 @@ links.forEach((val, index)=>{
     val.addEventListener('mouseleave',()=>{
         divs[index].classList.add('none');
     })
+})
+
+ham.addEventListener('click',()=>{
+    document.body.querySelector('.main').classList.add('black');
+    login.classList.remove('hw-none');
+    login.classList.add('hw-full');
+})
+
+close.addEventListener('click', ()=>{
+    document.body.querySelector('.main').classList.remove('black');
+    login.classList.remove('hw-full');
+    login.classList.add('hw-none');
 })
